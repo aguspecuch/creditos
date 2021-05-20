0e028e6ea558b1261a7c22bdf66c6ded5a71da1d
aguspecuch <agustina.pecuch@hotmail.com>
2021-05-20 10:47:20 -0300

    Agrego los metodos de ClienteManager y ProgramaCredito.

--- src/main/java/ar/com/ada/creditos/App.java
+++ src/main/java/ar/com/ada/creditos/App.java
@@ -6,8 +6,10 @@ package ar.com.ada.creditos;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        
+        ProgramaCredito miProgramaCredito = new ProgramaCredito();
+
+        miProgramaCredito.iniciar();
     }
 }
--- src/main/java/ar/com/ada/creditos/Credito.java
+++
@@ -1,5 +0,0 @@
-package ar.com.ada.creditos;
-
-public class Credito {
-    
-}
